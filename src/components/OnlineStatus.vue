84b2430a5ac55437d1c1ba4d450c9632a85fa8b1
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2025-11-02 08:22:23 +0000

    feat add request recevied in online status card

--- src/components/OnlineStatus.vue
+++ src/components/OnlineStatus.vue
@@ -1,57 +1,58 @@
 <template>
-  <AppCard>
-    <template #body>
-      <div class="text-h6">
-        Status
-      </div>
-      <section class="my-1">
-        See adsbdb <ExternalLink href="https://www.twitter.com/adsbdb" text="twitter page" /> for any status updates.
-        <br>
-        Please report any incorrect data to the
-        <ExternalLink :href text="issues page" /> with the <span
-          class="font-weight-bold"
-        >Data</span> tag.
-      </section>
-      <v-row v-if="api_version" class="ma-0 pa-0" justify="center">
-        <v-col class="ma-0 pa-0 my-3" cols="12">
-          <GenericTable no-headers :rows />
-        </v-col>
-      </v-row>
-      <v-skeleton-loader v-else type="table-row-divider, table-row" />
-    </template>
-  </AppCard>
+	<AppCard>
+		<template #body>
+			<div class='text-h6'>
+				Status
+			</div>
+			<section class='my-1'>
+				See adsbdb <ExternalLink href='https://www.twitter.com/adsbdb' text='twitter page' /> for any status updates.
+				<br>
+				Please report any incorrect data to the
+				<ExternalLink :href text='issues page' /> with the <span
+					class='font-weight-bold'
+				>Data</span> tag.
+			</section>
+			<v-row v-if='api_version' class='ma-0 pa-0' justify='center'>
+				<v-col class='ma-0 pa-0 my-3' cols='12'>
+					<GenericTable no-headers :rows />
+				</v-col>
+			</v-row>
+			<v-skeleton-loader v-else type='table-row-divider, table-row' />
+		</template>
+	</AppCard>
 </template>
 
 <script setup lang="ts">
-  import { apiRequests } from '@/services/axios'
-  import { env } from '@/services/env'
-  import { secondsToText } from '@/services/text'
+import { apiRequests } from '@/services/axios'
+import { env } from '@/services/env'
+import { secondsToText } from '@/services/text'
 
-  const href = `${env.github_homepage}/adsbdb/issues`
+const href = `${env.github_homepage}/adsbdb/issues`
 
-  const onlineStore = onlineModule()
-  const api_version = computed((): string => onlineStore.api_version)
-  const online_time = computed({
-    get (): string {
-      return secondsToText(onlineStore.online_time)
-    },
-    set (b: number): void {
-      onlineStore.set_online_time(b)
-    },
-  })
+const onlineStore = onlineModule()
+const api_version = computed((): string => onlineStore.api_version)
+const online_time = computed({
+	get (): string {
+		return secondsToText(onlineStore.online_time)
+	},
+	set (b: number): void {
+		onlineStore.set_online_time(b)
+	},
+})
 
-  const rows = computed(() => [
-    { key: 'api_version', value: [api_version.value] },
-    { key: 'online for', value: [online_time.value] },
-  ])
+const rows = computed(() => [
+	{ key: 'api_version', value: [api_version.value] },
+	{ key: 'online for', value: [online_time.value] },
+	{ key: 'requests recieved', value: [onlineStore.stats?.response.total.aggregate.toLocaleString() ?? '0'] },
+])
 
-  onMounted(async () => {
-    const { api_version, uptime } = await (await apiRequests.online_get()).response
-    online_time.value = uptime
-    onlineStore.set_api_version(api_version)
-    setInterval(() => {
-      onlineStore.increase_online_time()
-    }, 1000)
-  })
+onMounted(async () => {
+	const { api_version, uptime } = await (await apiRequests.online_get()).response
+	online_time.value = uptime
+	onlineStore.set_api_version(api_version)
+	setInterval(() => {
+		onlineStore.increase_online_time()
+	}, 1000)
+})
 
 </script>
--- src/store/online.ts
+++ src/store/online.ts
@@ -1,21 +1,25 @@
 import { defineStore } from 'pinia'
-import { ModuleName } from '@/types'
+import { type ApiResponse, ModuleName, type StatsResponse } from '@/types'
 
 export const onlineModule = defineStore(ModuleName.Online, {
-  state: () => ({
-    api_version: '',
-    online_time: 0,
-  }),
+	state: () => ({
+		api_version: '',
+		online_time: 0,
+		stats: undefined as undefined | ApiResponse<StatsResponse>,
+	}),
 
-  actions: {
-    set_online_time (value: number) {
-      this.online_time = value
-    },
-    set_api_version (value: string) {
-      this.api_version = value
-    },
-    increase_online_time () {
-      this.online_time++
-    },
-  },
+	actions: {
+		set_online_time (value: number) {
+			this.online_time = value
+		},
+		set_api_version (value: string) {
+			this.api_version = value
+		},
+		set_stats (value: ApiResponse<StatsResponse>) {
+			this.stats = value
+		},
+		increase_online_time () {
+			this.online_time++
+		},
+	},
 })
